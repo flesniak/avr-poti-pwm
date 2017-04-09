4720d32eb438528e5b50cd64e0cfed874b6677e7
Fabian Lesniak <fabian@lesniak-it.de>
2017-04-09 12:48:13 +0200

    generate-lut.py: variable lut size, bitshift adc->lut, doc

--- generate-lut.py
+++ generate-lut.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-from math import exp, log
+from math import exp, log, log2, ceil
 
 current_target=0.6
 shunt_ohm=1.8/4
 vref=1.1
 adc_max=1023
-lut_size=64
+lut_size=256
 
+# for low_values_fixup, DEPRECATED
 # current sensing only works for sense_targets > 21 (duty cycle 30 = ~50mA)
 # all lower sense_targets will result in the same current
 min_measurable_current=0.05 # current below this value will be set to pwm without current measure
@@ -19,7 +20,9 @@ sense_target_int=round(sense_target)
 print("#include <avr/pgmspace.h>\n")
 print("#define SENSE_TARGET {}".format(sense_target_int))
 print("#define LUT_SIZE {}".format(lut_size))
+print("#define ADC_SHIFT_BITS {}".format(ceil(log2(adc_max+1))-ceil(log2(lut_size))))
 
+# exponential growth
 # 0 at 0, sense_target at lut_size-1
 def exp_lut(lut_size):
   lut = []
@@ -28,6 +31,7 @@ def exp_lut(lut_size):
     lut.append(round(exp(b*div)-1))
   return lut
 
+# parabolic growth
 # 0 at 0, sense_target at lut_size-1, 1 at 1
 def para_lut(lut_size):
   lut = []
@@ -37,6 +41,7 @@ def para_lut(lut_size):
     lut.append(round(a*x*x+b*x))
   return lut
 
+# linear growth
 def linear_lut(lut_size):
   lut = []
   for div in range(0,lut_size):
@@ -55,11 +60,11 @@ def low_values_fixup(lut):
   return limit_index
 
 #lut = linear_lut(lut_size)
-#lut = exp_lut(lut_size)
-lut = para_lut(lut_size)
+lut = exp_lut(lut_size)
+#lut = para_lut(lut_size)
 #print("#define DIRECT_PWM_LIMIT {}\n".format(low_values_fixup(lut)))
 print("#define DIRECT_PWM_LIMIT 0\n") # disables low value direct pwm
-print("const uint8_t brightness_lut[64] PROGMEM = {")
+print("const uint8_t brightness_lut[{}] PROGMEM = {{".format(lut_size))
 for index, chunk in enumerate([lut[i:i+10] for i in range(0, len(lut), 10)]):
   print("  "+",".join(str(v).rjust(3) for v in chunk)+("," if index < lut_size/10-1 else "")) # "0x{:02x}".format(v)
 print("};")
